d5f2d6cc3af2bd835b01d8bab793f90498f1a6df
Rea Sand <hekmek@posteo.de>
2019-02-19 17:06:47 +0100

    feat: PatternsAdapter - introduce pipeTo
    Co-authored-by: Moritz Maisel <maisel@sipgate.de>

--- src/main/java/io/retel/ariproxy/akkajavainterop/PatternsAdapter.java
+++ src/main/java/io/retel/ariproxy/akkajavainterop/PatternsAdapter.java
@@ -2,7 +2,9 @@ package io.retel.ariproxy.akkajavainterop;
 
 import akka.actor.ActorRef;
 import akka.pattern.Patterns;
+import akka.pattern.PipeToSupport.PipeableFuture;
 import io.vavr.concurrent.Future;
+import scala.concurrent.ExecutionContext;
 
 public class PatternsAdapter {
 
@@ -14,5 +16,9 @@ public class PatternsAdapter {
 				timeoutMillis
 		))
 				.map(o -> (T) o);
+	}
+
+	public static <T> PipeableFuture<T> pipeTo(Future<T> future, ActorRef replyTo, ExecutionContext executionContext) {
+		return Patterns.pipe(CustomFutureConverters.toScala(future), executionContext).to(replyTo);
 	}
 }

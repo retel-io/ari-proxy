0d2e4fa5f4534077867cbe5a9de785f01ed15140
Marc Löhe <bfncs@users.noreply.github.com>
2020-11-17 11:29:09 +0100

    feat: include related resource types and ids in responses and events (#48)

---
+++ src/main/java/io/retel/ariproxy/boundary/commandsandresponses/auxiliary/AriResource.java
@@ -0,0 +1,71 @@
+package io.retel.ariproxy.boundary.commandsandresponses.auxiliary;
+
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vavr.Value;
+import io.vavr.control.Option;
+import java.util.function.Function;
+
+public class AriResource {
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+  private final AriResourceType type;
+  private final String id;
+
+  public AriResource(final AriResourceType type, final String id) {
+    this.type = type;
+    this.id = id;
+  }
+
+  public static Option<AriResource> ofAriCommand(final AriCommand ariCommand) {
+    final AriCommandType type = ariCommand.extractCommandType();
+
+    if (type == AriCommandType.UNKNOWN) {
+      return Option.none();
+    }
+
+    final Option<String> maybeResourceId =
+        type.extractResourceIdFromUri(ariCommand.getUrl())
+            .orElse(
+                () -> {
+                  try {
+                    return type.extractResourceIdFromBody(
+                            OBJECT_MAPPER.writeValueAsString(ariCommand.getBody()))
+                        .map(Value::toOption)
+                        .flatMap(Function.identity());
+                  } catch (JsonProcessingException e) {
+                    throw new IllegalStateException("Unable to deserialize json", e);
+                  }
+                });
+
+    return maybeResourceId.flatMap(
+        resourceId -> Option.some(new AriResource(type.getResourceType(), resourceId)));
+  }
+
+  public AriResourceType getType() {
+    return type;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public String toString() {
+    return reflectionToString(this, SHORT_PREFIX_STYLE);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    return reflectionEquals(this, o);
+  }
+
+  @Override
+  public int hashCode() {
+    return reflectionHashCode(this);
+  }
+}
--- src/test/java/io/retel/ariproxy/boundary/commandsandresponses/AriCommandResponseProcessingTest.java
+++ src/test/java/io/retel/ariproxy/boundary/commandsandresponses/AriCommandResponseProcessingTest.java
@@ -1,20 +1,17 @@
 package io.retel.ariproxy.boundary.commandsandresponses;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import io.retel.ariproxy.boundary.callcontext.api.RegisterCallContext;
 import io.retel.ariproxy.boundary.commandsandresponses.auxiliary.AriCommand;
-import io.vavr.control.Either;
+import io.vavr.control.Try;
 import java.io.IOException;
 import java.time.Duration;
 import org.junit.jupiter.api.AfterEach;
@@ -23,103 +20,96 @@ import org.junit.jupiter.api.Test;
 
 class AriCommandResponseProcessingTest {
 
-	private final String TEST_SYSTEM = this.getClass().getSimpleName();
-	private ActorSystem system;
-	private static final ObjectMapper mapper = new ObjectMapper();
-	private static final ObjectReader ariCommandReader = mapper.readerFor(AriCommand.class);
+  private final String TEST_SYSTEM = this.getClass().getSimpleName();
+  private ActorSystem system;
+  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final ObjectReader ariCommandReader = mapper.readerFor(AriCommand.class);
 
+  @AfterEach
+  void teardown() {
+    TestKit.shutdownActorSystem(system);
+    system.terminate();
+  }
 
-	@AfterEach
-	void teardown() {
-		TestKit.shutdownActorSystem(system);
-		system.terminate();
-	}
+  @BeforeEach
+  void setup() {
+    system = ActorSystem.create(TEST_SYSTEM);
+  }
 
-	@BeforeEach
-	void setup() {
-		system = ActorSystem.create(TEST_SYSTEM);
-	}
+  @Test
+  void registerCallContextDoesNothingWhenItShouldnt() {
+    final TestKit callContextProvider = new TestKit(system);
+    final AriCommand ariCommand = new AriCommand(null, "/channels/CHANNEL_ID/answer", null);
 
-	@Test
-	void registerCallContextDoesNothingWhenItShouldnt() {
-		final TestKit callContextProvider = new TestKit(system);
-		final AriCommand ariCommand = mock(AriCommand.class);
-		doReturn("/channels/CHANNEL_ID/answer").when(ariCommand).getUrl();
-		doReturn(mock(JsonNode.class)).when(ariCommand).getBody();
+    final Try<Void> res =
+        AriCommandResponseProcessing.registerCallContext(
+            callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
 
-		final Either<RuntimeException, Runnable> res = AriCommandResponseProcessing
-				.registerCallContext(callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
+    assertTrue(res.isSuccess());
+    callContextProvider.expectNoMessage(Duration.ofMillis(500));
+  }
 
-		res.get().run();
+  @Test
+  void registerCallContextRegistersANewCallContextIfTheAriCommandTypeNecessitatesIt() {
+    final TestKit callContextProvider = new TestKit(system);
+    final AriCommand ariCommand =
+        new AriCommand(null, "/channels/CHANNEL_ID/play/PLAYBACK_ID", null);
 
-		callContextProvider.expectNoMessage(Duration.ofMillis(500));
-	}
+    final Try<Void> res =
+        AriCommandResponseProcessing.registerCallContext(
+            callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
 
-	@Test
-	void registerCallContextRegisteresANewCallContextIfTheAriCommandTypeNecessitatesIt() {
-		final TestKit callContextProvider = new TestKit(system);
-		final AriCommand ariCommand = mock(AriCommand.class);
-		doReturn("/channels/CHANNEL_ID/play/PLAYBACK_ID").when(ariCommand).getUrl();
-		doReturn(mock(JsonNode.class)).when(ariCommand).getBody();
+    assertTrue(res.isSuccess());
 
-		final Either<RuntimeException, Runnable> res = AriCommandResponseProcessing
-				.registerCallContext(callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
+    final RegisterCallContext registerCallContext =
+        callContextProvider.expectMsgClass(RegisterCallContext.class);
+    assertThat(registerCallContext.resourceId(), is("PLAYBACK_ID"));
+    assertThat(registerCallContext.callContext(), is("CALL_CONTEXT"));
+  }
 
-		res.get().run();
+  @Test
+  void registerCallContextThrowsARuntimeExceptionIfTheAriCommandIsMalformed() {
+    final AriCommand ariCommand = new AriCommand(null, "/channels", null);
+    final Try<Void> res = AriCommandResponseProcessing.registerCallContext(null, null, ariCommand);
 
-		final RegisterCallContext registerCallContext = callContextProvider.expectMsgClass(RegisterCallContext.class);
-		assertThat(registerCallContext.resourceId(), is("PLAYBACK_ID"));
-		assertThat(registerCallContext.callContext(), is("CALL_CONTEXT"));
-	}
+    assertTrue(res.isFailure());
+  }
 
-	@Test
-	void registerCallContextThrowsARuntimeExceptionIfTheAriCommandIsMalformed() {
-		final AriCommand ariCommand = mock(AriCommand.class);
-		doReturn("/channels").when(ariCommand).getUrl();
-		doReturn(null).when(ariCommand).getBody();
+  @Test
+  void ensureFallBackToBodyExtractorWorksAsExpected() throws IOException {
+    final TestKit callContextProvider = new TestKit(system);
+    final String json =
+        "{ \"method\":\"POST\", \"url\":\"/channels/CHANNEL_ID/record\", \"body\":{\"name\":\"RECORD_NAME\"}}";
+    final AriCommand ariCommand = ariCommandReader.readValue(json);
 
-		final Either<RuntimeException, Runnable> res = AriCommandResponseProcessing.registerCallContext(null, null, ariCommand);
+    final Try<Void> res =
+        AriCommandResponseProcessing.registerCallContext(
+            callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
 
-		assertThat(res.getLeft(), instanceOf(RuntimeException.class));
-		assertThat(res.getLeft().getMessage(), containsString("Failed to extract resourceId from both"));
-	}
+    assertTrue(res.isSuccess());
 
-	@Test
-	void ensureFallBackToBodyExtractorWorksAsExpected() throws IOException {
-		final TestKit callContextProvider = new TestKit(system);
-		final String json = "{ \"method\":\"POST\", \"url\":\"/channels/CHANNEL_ID/record\", \"body\":{\"name\":\"RECORD_NAME\"}}";
-		final AriCommand ariCommand = ariCommandReader.readValue(json);
+    final RegisterCallContext registerCallContext =
+        callContextProvider.expectMsgClass(RegisterCallContext.class);
+    assertEquals("RECORD_NAME", registerCallContext.resourceId());
+    assertEquals("CALL_CONTEXT", registerCallContext.callContext());
+  }
 
-		System.out.println(ariCommand);
+  @Test
+  void ensureFallBackToBodyExtractorWorksAsExpectedForChannelCreate() throws IOException {
+    final TestKit callContextProvider = new TestKit(system);
+    final String json =
+        "{ \"method\":\"POST\", \"url\":\"/channels/create\", \"body\":{\"channelId\":\"channel-Id\"}}";
+    final AriCommand ariCommand = ariCommandReader.readValue(json);
 
-		final Either<RuntimeException, Runnable> res = AriCommandResponseProcessing
-				.registerCallContext(callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
+    final Try<Void> res =
+        AriCommandResponseProcessing.registerCallContext(
+            callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
 
-		res.get().run();
+    assertTrue(res.isSuccess());
 
-		final RegisterCallContext registerCallContext = callContextProvider.expectMsgClass(RegisterCallContext.class);
-		assertThat(registerCallContext.resourceId(), is("RECORD_NAME"));
-		assertThat(registerCallContext.callContext(), is("CALL_CONTEXT"));
-
-	}
-
-	@Test
-	void ensureFallBackToBodyExtractorWorksAsExpectedForChannelCreate() throws IOException {
-		final TestKit callContextProvider = new TestKit(system);
-		final String json = "{ \"method\":\"POST\", \"url\":\"/channels/create\", \"body\":{\"channelId\":\"channel-Id\"}}";
-		final AriCommand ariCommand = ariCommandReader.readValue(json);
-
-		System.out.println(ariCommand);
-
-		final Either<RuntimeException, Runnable> res = AriCommandResponseProcessing
-				.registerCallContext(callContextProvider.getRef(), "CALL_CONTEXT", ariCommand);
-
-		res.get().run();
-
-		final RegisterCallContext registerCallContext = callContextProvider.expectMsgClass(RegisterCallContext.class);
-		assertThat(registerCallContext.resourceId(), is("channel-Id"));
-		assertThat(registerCallContext.callContext(), is("CALL_CONTEXT"));
-
-	}
-
+    final RegisterCallContext registerCallContext =
+        callContextProvider.expectMsgClass(RegisterCallContext.class);
+    assertThat(registerCallContext.resourceId(), is("channel-Id"));
+    assertThat(registerCallContext.callContext(), is("CALL_CONTEXT"));
+  }
 }
